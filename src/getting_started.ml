51f17cb16594a8f1d8b47b24608af29cf604eb02
Tadayoshi Sato <tadayosi@gmail.com>
2015-03-23 22:32:56 +0900

    Added chapter code in Chapter 2

--- src/getting_started.ml
+++ src/getting_started.ml
@@ -1,3 +1,13 @@
+let abs n =
+  if n < 0 then -n
+  else n
+
+let factorial n =
+  let rec go n acc =
+    if n <= 0 then acc
+    else go (n - 1) n * acc in
+  go n 1
+
 (* ex 2.1 *)
 let fib n =
   let rec f curr next = function
@@ -6,6 +16,13 @@ let fib n =
     | n' -> f next (curr + next) (n' - 1) in
   f 0 1 n
 
+let find_first xs p =
+  let rec loop n =
+    if n >= Array.length xs then -1
+    else if p xs.(n) then n
+    else loop (n + 1) in
+  loop 0
+
 (* ex 2.2 *)
 let is_sorted xs ordered =
   let rec f n =
@@ -13,6 +30,9 @@ let is_sorted xs ordered =
     else if not (ordered xs.(n) xs.(n + 1)) then false
     else f (n + 1) in
   f 0
+
+let partial1 a f =
+  fun b -> f a b
 
 (* ex 2.3 *)
 let curry f = fun a -> fun b -> f (a, b)
--- test/getting_started_test.ml
+++ test/getting_started_test.ml
@@ -2,9 +2,19 @@ open OUnit2
 open Getting_started
 
 let tests = "Chapter 2" >::: [
+  "abs" >::
+    (fun _ -> assert_equal 42 (abs (-42)));
+
+  "factorial" >::
+    (fun _ -> assert_equal 120 (factorial 5));
+
   "fib" >::
     (fun _ -> assert_equal [0; 1; 1; 2; 3; 5]
       (List.map fib [0; 1; 2; 3; 4; 5]));
+
+  "find_first" >::
+    (fun _ -> assert_equal 1
+      (find_first [|7; 9; 13|] (fun x -> x = 9)));
 
   "is_sorted true" >::
     (fun _ -> assert_equal true
@@ -12,6 +22,9 @@ let tests = "Chapter 2" >::: [
   "is_sorted false" >::
     (fun _ -> assert_equal false
       (is_sorted [|3; 1; 2; 5; 4|] (fun x y -> x <= y)));
+
+  "partial1" >::
+    (fun _ -> assert_equal 3 ((partial1 1 (+)) 2));
 
   "curry" >::
     (fun _ -> assert_equal 3
