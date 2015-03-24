ffeedfd719d808ea52aa88071c26c163885b544b
Tadayoshi Sato <tadayosi@gmail.com>
2015-03-24 12:09:09 +0900

    removed ~n from props

--- test/data_structures_test.ml
+++ test/data_structures_test.ml
@@ -2,18 +2,18 @@ open OUnit2
 open QCheck
 open Data_structures
 
-let sum_prop = mk_test ~n:100 ~name:"sum"
+let sum_prop = mk_test ~name:"sum"
   Arbitrary.(triple small_int small_int small_int)
   (fun (a, b, c) -> sum(Cons(a, Cons(b, Cons(c, Nil)))) = a + b + c)
 
-let product_prop = mk_test ~n:100 ~name:"product"
+let product_prop = mk_test ~name:"product"
   Arbitrary.(triple (float 10.0) (float 10.0) (float 10.0))
   (fun (a, b, c) ->
     let res = product(Cons(a, Cons(b, Cons(c, Nil)))) in
     let act = a *. b *. c in
     act -. 0.01 < res && res < act +. 0.01)
 
-let list_prop = mk_test ~n:100 ~name:"list"
+let list_prop = mk_test ~name:"list"
   Arbitrary.(triple small_int small_int small_int)
   (fun (a, b, c) -> list [a; b; c] = Cons(a, Cons(b, Cons(c, Nil))))
 
