14043859f59a857becd9f273165654340e90a428
Tadayoshi Sato <tadayosi@gmail.com>
2015-03-24 15:40:36 +0900

    3.3.1

--- src/data_structures.ml
+++ src/data_structures.ml
@@ -3,15 +3,29 @@ module List = struct
 
   let rec list xs = match xs with
     | [] -> Nil
-    | x :: xs' -> Cons(x, list xs')
+    | x :: xs' -> Cons (x, list xs')
 
-  let rec tail xs = match xs with
+  let tail xs = match xs with
     | Nil -> Nil
-    | Cons(_, xs') -> xs'
+    | Cons (_, xs') -> xs'
 
-  let rec set_head xs h = match xs with
-    | Nil -> Cons(h, Nil)
-    | Cons(_, xs') -> Cons(h, xs')
+  let rec drop xs n =
+    if n <= 0 then xs
+    else match xs with
+      | Nil -> Nil
+      | Cons (_, xs') -> drop xs' (n - 1)
+
+  let rec drop_while xs f = match xs with
+    | Cons (x, xs') when f x -> drop_while xs' f
+    | _ -> xs
+
+  let rec append xs ys = match xs with
+    | Nil -> ys
+    | Cons (x, xs') -> Cons (x, append xs' ys)
+
+  let set_head xs h = match xs with
+    | Nil -> Cons (h, Nil)
+    | Cons (_, xs') -> Cons (h, xs')
 
   let rec sum ints = match ints with
     | Nil -> 0
--- test/data_structures_test.ml
+++ test/data_structures_test.ml
@@ -4,7 +4,7 @@ open Data_structures.List
 
 let list_prop = mk_test ~name:"list"
   Arbitrary.(triple small_int small_int small_int)
-  (fun (a, b, c) -> list [a; b; c] = Cons(a, Cons(b, Cons(c, Nil))))
+  (fun (a, b, c) -> list [a; b; c] = Cons (a, Cons (b, Cons (c, Nil))))
 
 let tail_prop = mk_test ~name:"tail" ~pp:PP.(list int)
   Arbitrary.(list small_int)
@@ -12,26 +12,43 @@ let tail_prop = mk_test ~name:"tail" ~pp:PP.(list int)
     | [] -> tail (list xs) = Nil
     | _ -> tail (list xs) = list (List.tl xs))
 
+let drop_prop = mk_test ~name:"drop" ~pp:PP.(list int)
+  Arbitrary.(list small_int)
+  (fun xs -> drop (list xs) 3 = tail (tail (tail (list xs))))
+
+let drop_while_prop = mk_test ~name:"drop_while" ~pp:PP.(list int)
+  Arbitrary.(list small_int)
+  Prop.(
+    (fun xs -> drop_while (list xs) (fun _ -> true) = Nil) &&&
+    (fun xs -> drop_while (list xs) (fun _ -> false) = (list xs)))
+
+let append_prop = mk_test ~name:"append" ~pp:PP.(pair (list int) (list int))
+  Arbitrary.(pair (list small_int) (list small_int))
+  (fun (xs, ys) -> append (list xs) (list ys) = list (xs @ ys))
+
 let set_head_prop = mk_test ~name:"set_head" ~pp:PP.(list int)
   Arbitrary.(list small_int)
   (fun xs -> match xs with
-    | [] -> set_head (list xs) 1 = Cons(1, Nil)
+    | [] -> set_head (list xs) 1 = Cons (1, Nil)
     | _ -> set_head (list xs) 1 = list (1 :: List.tl xs))
 
 let sum_prop = mk_test ~name:"sum"
   Arbitrary.(triple small_int small_int small_int)
-  (fun (a, b, c) -> sum(Cons(a, Cons(b, Cons(c, Nil)))) = a + b + c)
+  (fun (a, b, c) -> sum(Cons (a, Cons (b, Cons (c, Nil)))) = a + b + c)
 
 let product_prop = mk_test ~name:"product"
   Arbitrary.(triple (float 10.0) (float 10.0) (float 10.0))
   (fun (a, b, c) ->
-    let res = product(Cons(a, Cons(b, Cons(c, Nil)))) in
+    let res = product(Cons (a, Cons (b, Cons (c, Nil)))) in
     let act = a *. b *. c in
     act -. 0.01 < res && res < act +. 0.01)
 
 let props = [
   list_prop;
   tail_prop;
+  drop_prop;
+  drop_while_prop;
+  append_prop;
   set_head_prop;
   sum_prop;
   product_prop;
@@ -41,9 +58,9 @@ let tests = "Chapter 3" >::: [
   "ex 3.1" >::
     (fun _ -> assert_equal 3
       (match list [1; 2; 3; 4; 5] with
-        | Cons(x, Cons(2, Cons(4, _))) -> x
+        | Cons (x, Cons (2, Cons (4, _))) -> x
         | Nil -> 42
-        | Cons(x, Cons(y, Cons(3, Cons(4, _)))) -> x + y
-        | Cons(h, t) -> h + sum t
+        | Cons (x, Cons (y, Cons (3, Cons (4, _)))) -> x + y
+        | Cons (h, t) -> h + sum t
         | _ -> 101));
 ]
