0180c8dc6636d8882c3a794a48640c82e74d9ba0
Tadayoshi Sato <tadayosi@gmail.com>
2015-03-25 18:08:39 +0900

    Swapped order of QCheck and oUnit

--- test/tests.ml
+++ test/tests.ml
@@ -1,12 +1,13 @@
 open OUnit2
 open QCheck
 
+(* QCheck properties *)
+let _ = run_tests
+  Data_structures_test.props
+
 (* OUnit tests *)
 let _ = run_test_tt_main ( "All tests" >::: [
   Getting_started_test.tests;
   Data_structures_test.tests;
 ])
 
-(* QCheck properties *)
-let _ = run_tests
-  Data_structures_test.props
